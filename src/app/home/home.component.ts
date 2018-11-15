7a7fee933f831b7dcd3ef283983edb503d210a7e
Joe Scharnitzke <j.scharnitzke@gmail.com>
2018-11-14 17:01:41 -0800

    basic observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    const myNumbers = Observable.interval(1000);
 
+    myNumbers.subscribe((number: number) => {
+      console.log(number);
+    });
+  }
 }
